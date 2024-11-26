c7b37882da10b11dabc995077a20be8f00d6fc9b
Akbar <akbarkhawaja321@gmail.com>
2024-11-25 21:28:20 -0800

    Fxed bug

--- Task-2-Task-Manager/backend/server.js
+++ Task-2-Task-Manager/backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://zephyrusAdmin:kGdMN9WhDPGUU2Bu@zephyruscluster.1ug59.mongodb.net/';
 
 const connectDB = async () => {
 	try {
