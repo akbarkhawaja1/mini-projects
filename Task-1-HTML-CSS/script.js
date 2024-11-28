d788ddb0949584279015937bb5a3e12aed61dce2
Akbar <akbarkhawaja321@gmail.com>
2024-11-27 22:46:24 -0800

    Add light mode text

--- Task-1-HTML-CSS/script.js
+++ Task-1-HTML-CSS/script.js
@@ -21,15 +21,21 @@ const body = document.body;
 
 if(localStorage.getItem('darkMode') === 'enabled'){
 	body.classList.add('dark-mode')
+	darkModeToggle.textContent = 'Light Mode'
+}
+else{
+	darkModeToggle.textContent = 'Dark Mode'
 }
 
 darkModeToggle.addEventListener('click', () =>{
 	body.classList.toggle('dark-mode')
 
 	if(body.classList.contains('dark-mode')){
+		darkModeToggle.textContent = 'Light Mode'
 		localStorage.setItem('darkMode', 'enabled')
 	}
 	else{
+		darkModeToggle.textContent = 'Dark Mode'
 		localStorage.setItem('darkMode', 'disabled')
 
 	}
